0d0c39f0e497d64789ef17b273f5d9ec4ffccac1
Bxk756 <225787637+Bxk756@users.noreply.github.com>
2025-12-13 18:29:41 +0000

    Explicitly disable Turbopack in Next 16

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => config,
+  // Explicitly disable Turbopack
+  turbopack: false,
+
+  webpack: (config) => {
+    return config;
+  },
 };
 
 module.exports = nextConfig;
